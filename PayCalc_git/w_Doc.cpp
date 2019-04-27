e0d940aa3afc5c3c1496932027cd42cfeefc3454
nano40375 <nano40375@konkuk.ac.kr>
2019-04-27 23:16:24 +0900

    메모리 저장부분 만드는중

--- PayCalc_git/w_Doc.cpp
+++ PayCalc_git/w_Doc.cpp
@@ -6,6 +6,16 @@ w_Doc::w_Doc()
 {
 }
 
+w_Doc::w_Doc(string name)
+	:w_name(name)
+{
+}
+
+void w_Doc::addRecord(string wDate, string iHour, string oHour)
+{
+	this->wDoc.push_back(wRecord(wDate, iHour, oHour));
+}
+
 
 w_Doc::~w_Doc()
 {
